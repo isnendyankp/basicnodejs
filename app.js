ec1be6179118d902fa42cc21f42d23b8be0d6fc7
isnendyan <isnendyankp@gmail.com>
2023-06-14 13:02:17 +0700

    S5-59: import express

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
 console.log(routes.someText);
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3000);
