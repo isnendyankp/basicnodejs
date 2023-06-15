342a37ea9d3f4c720d69800fdea86072464814c2
isnendyan <isnendyankp@gmail.com>
2023-06-15 13:51:21 +0700

    S5-60: Adding 1 middleware

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// npm start 
+
 const http = require('http');
 
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+});
+
+
 
 const server = http.createServer(app);
 
