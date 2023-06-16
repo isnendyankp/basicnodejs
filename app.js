55fa053e0291985e2e932cb8ad7f06e764e14034
isnendyan <isnendyankp@gmail.com>
2023-06-17 04:58:36 +0700

    s5-63: FInal handling different routes

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-// npm start for start beginning
-
-// s5-62: shorthand to set up the server so the constant no need it that syntax
-// const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     console.log('In the middleware!');
-//     next(); // Allows the request to continue to the next middleware in line
-// });
-
-// app.use((req, res, next) => {
-//     console.log('In another middleware!');
-//     res.send('<h1>Hello from Express!</h1>');
-// });
-
-// s5-63: Setup Handling different Routes/ multiple middleware
+// s5-63: Setup middleware for multiple routes
 app.use('/', (req, res, next) => {
     console.log('This always runs!');
     next();
@@ -29,15 +14,11 @@ app.use('/add-product', (req, res, next) => {
   res.send('<h1>The "Add Product" Page</h1>');
 });
 
-// s5-63: main middleware
+// s5-63: final 
 app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
 
 // s5-62: shorthand to set up the server
 app.listen(3000);
