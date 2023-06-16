c13f6608923f7a72d4151c50711577277da295cd
isnendyan <isnendyankp@gmail.com>
2023-06-16 13:50:16 +0700

    s5-63: main middleware

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use('/add-product', (req, res, next) => {
   res.send('<h1>The "Add Product" Page</h1>');
 });
 
+// s5-63: main middleware
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
 // const server = http.createServer(app);
 
 // server.listen(3000);
