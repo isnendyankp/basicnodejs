e0e26bef52c1ff65d0678693db87ec8e85e65429
isnendyan <isnendyankp@gmail.com>
2023-06-16 13:47:17 +0700

    s5-63:  add product for different routes

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use('/', (req, res, next) => {
     next();
 });
 
+// s5-63:  add product for different routes
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
 // const server = http.createServer(app);
 
 // server.listen(3000);
