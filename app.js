9b87b532157dfe6081b1d4d78707975067605150
isnendyan <isnendyankp@gmail.com>
2023-06-19 13:54:39 +0700

    s5-64: change add product return to form

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/', (req, res, next) => {
 // s5-63:  add product for different routes
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 app.use('/', (req, res, next) => {
