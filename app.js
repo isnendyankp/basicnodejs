cff3a21288d5db4bdcaff42e378ac5b6dbea3b8b
isnendyan <isnendyankp@gmail.com>
2023-06-20 13:33:36 +0700

    s5-66: import adminroutes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// import all routes folder
+const adminRoutes = require('./routes/admin');
+
 // install npm install --save body-parser for parsing
 // URL encoding is the process of converting non-alphanumeric characters into a computer-understandable representation of a URL.
 app.use(bodyParser.urlencoded({extended: false}));
