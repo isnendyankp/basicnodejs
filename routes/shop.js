1dd4d5025129a76dec9f0053c7c99b3efdb97a9b
isnendyan <isnendyankp@gmail.com>
2023-06-21 13:04:50 +0700

    s5-66: setup shop routes

---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+// import express
+const express = require('express');
+
+// cr8 router object by calling express.router as a function
+const router = express.Router();
+
+
+// export the router
+module.exports = router;
