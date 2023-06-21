be735d2ed833ac5f4e12a1e5e4983627f7ac9be6
isnendyan <isnendyankp@gmail.com>
2023-06-21 13:09:26 +0700

    s5-66: import shopRoutes on app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // import all routes folder
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // install npm install --save body-parser for parsing
 // URL encoding is the process of converting non-alphanumeric characters into a computer-understandable representation of a URL.
