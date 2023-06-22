70a6ee2d105d852e2455e235a71c760d5dc1b999
isnendyan <isnendyankp@gmail.com>
2023-06-22 13:14:53 +0700

    s5-67: Adding a 404 Error Page

--- routes/admin.js
+++ routes/admin.js
@@ -13,4 +13,9 @@ router.post('/product', (req, res, next) => {
   res.redirect('/');
 });
 
+// adding a 404 Error page
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 module.exports = router;
