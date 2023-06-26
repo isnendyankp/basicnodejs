19d3675608be06e40f0bbe9e97a0eaf8409c6420
isnendyankp <isnendyankp@gmail.com>
2023-06-26 15:00:42 +0700

    s5-70: sendfile with targeting add-product.html

--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,7 @@ const router = express.Router();
 // /admin/add-product => GEt
 // Cr8 filter path for admin to add product
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
