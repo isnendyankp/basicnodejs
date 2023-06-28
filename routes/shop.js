896feb6bbce1dc9eb13f5bf5367cf7c1a3d0eeaf
isnendyankp <isnendyankp@gmail.com>
2023-06-28 13:45:07 +0700

    s5-73: import util-path & cr8 rootDir for access

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,9 @@ const path = require('path');
 // import express
 const express = require('express');
 
+// import util-path & cr8 rootDir for easy access navigation sendfile
+const rootDir = require('../util/path');
+
 // cr8 router object by calling express.router as a function
 const router = express.Router();
 
@@ -13,7 +16,7 @@ router.get('/', (req, res, next) => {
   // res.sendFile() to send a file as a response to an HTTP request.
   // path.join() is a method commonly used in Node.js to join multiple path segments into a single path. 
   // build path with _dirname > go up on lvl with ../ > then go to views folder > target shop.html
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 // export the router
