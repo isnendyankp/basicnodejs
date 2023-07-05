94b0840e33350a03c43af3f6c08f0feaa630da8b
isnendyankp <isnendyankp@gmail.com>
2023-07-05 13:00:33 +0700

    s6-79: on post admin add products.push

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,8 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/product', (req, res, next) => {
-  console.log(req.body);
+  // push new element into this array, a new object
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
