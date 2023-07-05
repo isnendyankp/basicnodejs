88468c4a6a156d6ca85068b65a5b0b7b6b362b9d
isnendyankp <isnendyankp@gmail.com>
2023-07-05 12:57:20 +0700

    s6-79: fix code for import all routes & call routes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // import all routes folder
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // install npm install --save body-parser for parsing
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Serving File Staticly for split styling css
 app.use(express.static(path.join(__dirname, 'public')));
 // call Routes
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // adding a 404 Error page
