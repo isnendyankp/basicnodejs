83ccc52acb72efa61b33a5c256c3304d62f63822
isnendyankp <isnendyankp@gmail.com>
2023-07-24 16:58:05 +0700

    s6-92: add conditional check path on shop

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,14 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
   // access render template from main-layout.pug
   // add path for access render
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+  router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
