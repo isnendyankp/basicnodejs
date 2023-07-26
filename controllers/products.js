365f7e978bdadf9242c3c62714b88ea61560b7d4
isnendyankp <isnendyankp@gmail.com>
2023-07-26 08:08:06 +0700

    s7-98: move funct middleware get

---
+++ controllers/products.js
@@ -0,0 +1,11 @@
+(req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+// add middleware function admin add product
--- routes/admin.js
+++ routes/admin.js
@@ -10,17 +10,7 @@ const products = [];
 
 // /admin/add-product => GEt
 // Cr8 filter path for admin to add product
-router.get('/add-product', (req, res, next) => {
-  // access render template from main-layout.pug
-  // add path for access render
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', );
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
@@ -33,3 +23,4 @@ exports.routes = router;
 exports.products = products;
 
 // - add 3 property & make it true for render: formsCSS: true, productCSS: true, activeAddProduct: true
+// - move function middleware get on route add product
