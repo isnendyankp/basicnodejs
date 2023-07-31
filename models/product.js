6d65cc39f44bff17cd2f2c02b1bf5a5b5598bf42
isnendyankp <isnendyankp@gmail.com>
2023-07-31 08:19:15 +0700

    s7-100:Cr8 constructor with add property

--- models/product.js
+++ models/product.js
@@ -1,5 +1,8 @@
 module.exports = class Product {
-
+    constructor(t) {
+        this.title = t;
+    }
 }
 
 // - create base class product with export
+// - cr8 constructor with add property and make it to parameter
