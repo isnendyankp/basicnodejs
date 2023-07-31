dfaaecae9f8a441e4ccb423796b2d1f948a4bef9
isnendyankp <isnendyankp@gmail.com>
2023-07-31 08:24:29 +0700

    s7-100:cr8 base save method

--- models/product.js
+++ models/product.js
@@ -4,8 +4,13 @@ module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
+
+    save() {
+        
+    }
 }
 
 // - create base class product with export
 // - cr8 constructor with add property and make it to parameter
 // - cr8 product array
+// - cr8 base save method
