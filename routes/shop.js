5c8602d0e127827437b48ddc7457067bc44033c3
isnendyankp <isnendyankp@gmail.com>
2023-08-13 08:52:13 +0700

    s8-109:Tidy up code & remove junk code

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,11 @@
-// 1
 const path = require('path');
-// 2
+
 const express = require('express');
-// 3
-// const rootDir = require('../util/path');
-// 4
-// const adminData = require('./admin');
-// import products
+
 const shopController = require('../controllers/shop');
-// 5
+
 const router = express.Router();
-// 6
+
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
@@ -18,24 +13,24 @@ router.get('/products', shopController.getProducts);
 router.get('/cart', shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
-// 7
+
 module.exports = router;
 
 // progress:
-// 1. import path for res.sendFile
-// 2. import express
-// 3. import util-path & cr8 rootDir for easy access navigation sendfile.
-// 4. access to the product with import adminData.
-// 5. cr8 router object by calling express.router as a function
-// 6. The function router.get() in middleware is used to handle incoming GET HTTP requests in Express.js. : 
-// 6. rendering template shop with object contains data {prods: products} & pass in with additional key path: path: '/'
-// 7. export the router
-// 6. add new key value pair & pass into template : hasProducts: products.length > 0,
-// 6. add property activeShop & set it into true for pass from main layout.hbs
-// 6. add property productCSS & set it into true for pass from main layout.hbs 
-// 3&4. unenable rootDir & adminData
-// import with cr8 variable productsController
-// pass in productsController getProducts
-// Registering Routes: product,cart&checkout
+// - import path for res.sendFile
+// - import express
+// - import util-path & cr8 rootDir for easy access navigation sendfile.
+// - access to the product with import adminData.
+// - cr8 router object by calling express.router as a function
+// - The function router.get() in middleware is used to handle incoming GET HTTP requests in Express.js. : 
+// - rendering template shop with object contains data {prods: products} & pass in with additional key path: path: '/'
+// - export the router
+// - add new key value pair & pass into template : hasProducts: products.length > 0,
+// - add property activeShop & set it into true for pass from main layout.hbs
+// - add property productCSS & set it into true for pass from main layout.hbs 
+// - unenable rootDir & adminData
+// - import with cr8 variable productsController
+// - pass in productsController getProducts
+// - Registering Routes: product,cart&checkout
 // - change path router into shopController
 // - fix path all router controller for each part
