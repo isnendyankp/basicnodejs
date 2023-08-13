ecfe7551a3c19b9c934b3bc7e0fb23ac2ca3bc18
isnendyankp <isnendyankp@gmail.com>
2023-08-13 08:45:40 +0700

    s8-109:change controller path for getProducts

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products');
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
@@ -26,3 +26,4 @@ module.exports = router;
 // - export the router
 // - Registering Routes: admin>products
 // - change controller path on router into adminController
+// - change controller path for getProducts
