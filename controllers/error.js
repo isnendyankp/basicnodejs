05b78727eefb9d3107988c3c46e9fa7b7d6a0f0c
isnendyankp <isnendyankp@gmail.com>
2023-08-13 08:48:19 +0700

    s8-109:Add path 404 on render status 404

--- controllers/error.js
+++ controllers/error.js
@@ -1,8 +1,9 @@
 exports.get404 = (req, res, next) => {
     //  passing pageTitle: 'Page NotFound'
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
 
 
 // - add 404 controller function middleware
 // - export with cr8 varible get404
+// - Add path 404 on render status 404
