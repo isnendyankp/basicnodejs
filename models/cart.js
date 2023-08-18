5dff4781403e837012a3da1b2e963094c0b94c50
isnendyankp <isnendyankp@gmail.com>
2023-08-18 07:45:45 +0700

    s9-123:Add product property with type array

--- models/cart.js
+++ models/cart.js
@@ -1,8 +1,9 @@
 module.exports = class Cart {
     constructor() {
-
+        this.products = [];
     }
 }
 
 // - cr8 class cart model base
 // - add constructor base
+// - Add product property with type array
