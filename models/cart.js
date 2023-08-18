8292024aa2aaaad3e46f6f663f6671c22c095598
isnendyankp <isnendyankp@gmail.com>
2023-08-18 07:47:24 +0700

    s9-123:Add totalPrice property with equal 0

--- models/cart.js
+++ models/cart.js
@@ -1,9 +1,11 @@
 module.exports = class Cart {
     constructor() {
         this.products = [];
+        this.totalPrice = 0;
     }
 }
 
 // - cr8 class cart model base
 // - add constructor base
 // - Add product property with type array
+// - Add totalPrice property with equal 0
