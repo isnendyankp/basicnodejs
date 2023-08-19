298e9741060e15baeecf99738f8c42896eb5bdf6
isnendyankp <isnendyankp@gmail.com>
2023-08-19 07:36:26 +0700

    s9-123:Add cart.products after updateProduct

--- models/cart.js
+++ models/cart.js
@@ -26,6 +26,8 @@ module.exports = class Cart {
         if (existingProduct) {
             updatedProduct = { ...existingProduct };
             updatedProduct.qty = updatedProduct.qty + 1;
+            cart.products = [...cart.products];
+            cart.products[existingProductIndex] = updatedProduct;
         } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
@@ -53,3 +55,4 @@ module.exports = class Cart {
 // - s9-123:Add argument productPrice
 // - s9-123:Add spread operator on updateProduct at else statement
 // - s9-123:cr8 existingProductIndex for find existing product by index
+// - s9-123:Add cart.products after updateProduct in array
