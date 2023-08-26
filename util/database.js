109aaa7391f69f01d1b7f4a4aa33601a662e7d8a
isnendyankp <isnendyankp@gmail.com>
2023-08-26 11:26:02 +0700

    S10-140:Add information for pool

--- util/database.js
+++ util/database.js
@@ -1,7 +1,13 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'nodecomplete'
 });
 
-// cr8 database.js file & import mysql
+
+// - S10-140:Cr8 database.js file & import mysql
+// - S10-140:Cr8 base for mysql.createPool
+// - S10-140:Add information for pool
