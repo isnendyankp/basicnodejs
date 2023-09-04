a98b071f0d9d57320ba73848ce56ffa9027650f9
isnendyankp <isnendyankp@gmail.com>
2023-09-04 13:28:32 +0700

    S11-151: remove mysql2 connect for setup Sequelize

--- util/database.js
+++ util/database.js
@@ -1,15 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '#Semogasukses27'
+// });
+// module.exports = pool.promise();
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '#Semogasukses27'
-});
-
-module.exports = pool.promise();
+const Sequelize = require('sequelize');
 
 // - S10-140:Cr8 database.js file & import mysql
 // - S10-140:Cr8 base for mysql.createPool
 // - S10-140:Add information for pool
 // - S10-140:Export pool.promise
+// - S11-151: remove mysql2 connect for setup Sequelize
