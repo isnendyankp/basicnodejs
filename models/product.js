d13256721b6df678d699a9e8fdf35b24765fdc86
isnendyankp <isnendyankp@gmail.com>
2023-09-05 06:51:39 +0700

    S11-152:cr8 Product for define module by sequelize

--- models/product.js
+++ models/product.js
@@ -2,5 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
+const Product = sequelize.define()
+
 // - S11-152:cr8 Sequelize for import sequelize
 // - S11-152:import sequelize  database pool managed by sequelize
+// - cr8 Product for define module that will be managed by sequelize
