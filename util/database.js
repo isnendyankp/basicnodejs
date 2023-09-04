5aff0b3f6b0292e1276ced2dfafe6ff805f30b46
isnendyankp <isnendyankp@gmail.com>
2023-09-04 13:29:54 +0700

    S11-151: cr8 sequelize instant with call Sequelize

--- util/database.js
+++ util/database.js
@@ -9,8 +9,11 @@
 
 const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize();
+
 // - S10-140:Cr8 database.js file & import mysql
 // - S10-140:Cr8 base for mysql.createPool
 // - S10-140:Add information for pool
 // - S10-140:Export pool.promise
 // - S11-151: remove mysql2 connect for setup Sequelize
+// - S11-151: cr8 sequelize instant with call Sequelize
