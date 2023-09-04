a5e6ae9ba54a2ae72a4e1f6fdc5ffc8c9daba196
isnendyankp <isnendyankp@gmail.com>
2023-09-05 06:48:26 +0700

    S11-152:import sequelize  database pool managed

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
 const Sequelize = require('sequelize');
 
-// - 
+const sequelize = require('../util/database');
+
+// - S11-152:cr8 Sequelize for import sequelize
+// - S11-152:import sequelize  database pool managed by sequelize
