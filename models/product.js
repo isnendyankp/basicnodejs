78d6e59bf81658172b185a796f5b8bf883515f0d
isnendyankp <isnendyankp@gmail.com>
2023-09-05 06:52:54 +0700

    S11-152:Add product as a module name

--- models/product.js
+++ models/product.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Product = sequelize.define()
+const Product = sequelize.define('product')
 
 // - S11-152:cr8 Sequelize for import sequelize
 // - S11-152:import sequelize  database pool managed by sequelize
-// - cr8 Product for define module that will be managed by sequelize
+// - S11-152:cr8 Product for define module by sequelize
+// - S11-152:Add product as a module name
