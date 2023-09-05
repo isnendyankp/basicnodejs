de32bb6d21b5f87dc4dacad8311c0ce83eaf9544
isnendyankp <isnendyankp@gmail.com>
2023-09-05 13:36:20 +0700

    S11-152:Add title property

--- models/product.js
+++ models/product.js
@@ -9,6 +9,7 @@ const Product = sequelize.define('product',{
         allowNull: false,
         primaryKey: true
     },
+    title: Sequelize.STRING
 });
 
 // - S11-152:cr8 Sequelize for import sequelize
@@ -21,3 +22,4 @@ const Product = sequelize.define('product',{
 // - S11-152:Add autoIncrement for automatically increase every time a new product is added to the database.
 // - S11-152:Add allowNull false for indicates that the value of the 'id' column will be automatically incremented every time a new product is added to the database.
 // - S11-152:Add primaryKey true for used to uniquely identify each entity in the products table. By providing the id attribute as the primary key, Sequelize will manage it specifically in database operations.
+// - S11-152:Add title property  with type data string
