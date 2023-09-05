1703f5cb331fc8fc0d66e9118e383eaa3147e595
isnendyankp <isnendyankp@gmail.com>
2023-09-05 13:24:03 +0700

    S11-152:Setup second argument for structure

--- models/product.js
+++ models/product.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const Product = sequelize.define('product')
+const Product = sequelize.define('product',{
+
+});
 
 // - S11-152:cr8 Sequelize for import sequelize
 // - S11-152:import sequelize  database pool managed by sequelize
 // - S11-152:cr8 Product for define module by sequelize
 // - S11-152:Add product as a module name
+// - S11-152:Setup second argument for structure
