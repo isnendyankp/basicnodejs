3ede82a49afa54dd89dbbe41c7b2e5ec2d9b6b32
isnendyankp <isnendyankp@gmail.com>
2023-09-22 07:03:07 +0700

    S11-159:Add check err @postEditProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,7 +57,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   Product.findById()
   .then()
-  .catch();
+  .catch(err => console.log(err));
   res.redirect('/admin/products');
 };
 
@@ -128,3 +128,4 @@ exports.postDeleteProduct = (req, res, next) => {
 // - S11-159:Add function to find element 1st database @PostEditProduct
 // - S11-159:Add then promises @PostEditProduct>findById
 // - S11-159:Add catch promises @PostEditProduct>findById
+// - S11-159:Add check err @postEditProduct>findById>catch
