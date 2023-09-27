b6ad5e41a30928ecb5dae8e889845345d19a1400
isnendyankp <isnendyankp@gmail.com>
2023-09-27 09:16:03 +0700

    S11-160:Add then Prom@postDeleteProduct>findById

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,7 +84,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId);
+  Product.findById(prodId)
+    .then();
   res.redirect('/admin/products');
 };
 
@@ -151,3 +152,4 @@ exports.postDeleteProduct = (req, res, next) => {
 // - S11-159: Move ress redirect
 // - S11-160:change deleteById to findById @postDelete Product
 // - S11-160:Pass in prodId @postDeleteProduct>findById
+// - S11-160:Add then Prom@postDeleteProduct>findById
