2d8b48e5a89794d40666cac88cdff6ab2d7589b7
isnendyankp <isnendyankp@gmail.com>
2023-09-29 15:10:51 +0700

    S11-161: define sequelize on database

--- models/use.js
+++ models/use.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
+
 
 // progress:
-// S11-161: define sequelize
+// S11-161: define Sequelize
+// S11-161: define sequelize on database
