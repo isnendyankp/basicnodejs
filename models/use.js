7f11d2357a9814af71ad3e9a12ac49bc6423300b
isnendyankp <isnendyankp@gmail.com>
2023-09-29 15:13:45 +0700

    S11-161: Add structure of the user model

--- models/use.js
+++ models/use.js
@@ -2,10 +2,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const User = sequelize.define('user');
+const User = sequelize.define('user', {
+
+});
 
 
 // progress:
 // S11-161: define Sequelize
 // S11-161: define sequelize on database
 // S11-161: define User model
+// S11-161: Add structure of the user model
