145314b544191cf2089dfc2d311525c3b17adf7b
isnendyankp <isnendyankp@gmail.com>
2023-10-18 10:16:07 +0700

    S11-163:Add next for continue the next step

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
     User.findById(1)
         .then(user => {
             req.user = user;
+            next();
         });
 });
 
@@ -65,66 +66,3 @@ sequelize
     .catch(err => {
         console.log(err);
     });
-
-// Description:
-// - The app.set() used to set configuration options for the Express application.
-// -- have two arguments: settingName, settingValue
-// - app.set('view engine', 'pug');
-// -- set the default view engine to Pug.
-// -- Express will use Pug as the primary view engine for rendering the application's views.
-// - app.set('views', 'views');
-// -- function in Express.js is used to specify the directory where the view templates are located.
-// -- app.set('views = functiion make default folder view', 'directory folder view');
-// - app.engine(); cr8 function with engine method for using express js.
-// -- need two parameter : name engine, function rendering will calling with express.js
-// -- // - app.engine('handlebars', expressHbs());  handlebars: name, expressHbs(); function rendering
-// - app.set('view engine', 'handlebars'); switch view engine to handlebars
-// - pass option layoutsDir property on handlebars engine
-// - pass option defaultLayout property on handlebars engine
-// - set the extension name which only applies to the layout and not to all files
-// - unactive hbs & setup view engine into ejs
-// - change adminData into adminRoutes at import all routes folder
-// - change section call Routes adminData into adminRoutes
-// - move 404 controller function middleware to error.js
-// - import errorController
-// - pass in errorController to app.use
-// - S10-140:Import database
-// - S10-140:Add db.execute base which allow to use database
-// - S10-140:Add accessing tables product from mysql
-// - S10-141:Execute db with 2 function promises then & catch
-// - S10-142:Add argument err in funct catch & log err
-// - S10-142:Add argument result in funct then & log result
-// - S10-142:Edit log result(array with 2 nested element)
-// - S11-153:rename cb const to sequelize
-// - S11-153:Call sequelize base
-// - S11-153:Add then method on Call sequelize
-// - S11-153:Add result at then sequelize
-// - S11-153:Add catch base on call sequelize
-// - S11-153:Add parameter err on catch sequelize
-// - S11-153:Add log err on catch sequelize
-// - S11-153:Move setup the server in then sequelize
-// - S11-153:unactive log result in then sequelize
-// - S11-162:Import Product model
-// - S11-162:Import User model
-// - S11-162:Call sequelize base with belongTo method
-// - S11-162:Add User model on belongTo method
-// - S11-162:Add constraint true on belongTo method
-// - S11-162:Add onDelete method on belongTo method
-// - S11-162:Add define user hasMany
-// - S11-162:Add product @user>hasMany method
-// - S11-162:Add force true @sequelize>sync
-// - S11-163:unactive .sync force true @sequelize>sync
-// - S11-163:Add findById method on User @sequelize>sync>then>result
-// - S11-163:Change to return for User.findById(1) @sequelize>sync>then>result
-// - S11-163:Add then method for retrive user @sequelize
-// - S11-163:Add if condition for check if dont have a user @sequelize
-// - S11-163:Add user.create @sequelize>then
-// - S11-163:Add prop name & email for user.create @sequelize>then
-// - S11-163:Add return if condition for user.create @sequelize>then
-// - S11-163:Add return for then user @sequelize>then
-// - S11-163:Add then method for using app.listen @sequelize
-// - S11-163:Add user then method for app.listen @sequelize
-// - S11-163:Add log user @sequelize>then(app.listen)
-// - S11-163:passin app.listen into then method @sequelize(app.listen)
-// - S11-163:Add new middleware for user @app>use
-// - S11-163:Add findById method on User @app>use
