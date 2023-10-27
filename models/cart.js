ad424e052f03abd9305d6c1fe88ce310fe52d6b9
isnendyankp <isnendyankp@gmail.com>
2023-10-27 08:16:40 +0700

    Add id property to cart model

--- models/cart.js
+++ models/cart.js
@@ -3,7 +3,12 @@ const Seqeulize = require('sequelize');
 const sequelize = require('../util/database');
 
 const Cart = sequelize.define('cart', {
-
+    id: {
+        type: Seqeulize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
 });
 
 
@@ -11,3 +16,4 @@ const Cart = sequelize.define('cart', {
 // s11-166:Import Sequelize 
 // s11-166:Import sequelize from database file
 // s11-166:Define Cart model with sequelize.define() & name it cart
+// s11-166:Add id property to cart model
