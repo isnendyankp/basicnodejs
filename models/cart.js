6c774f01f7e9c2ad2979758aa84cdc01f26d8db6
isnendyankp <isnendyankp@gmail.com>
2023-10-27 08:14:06 +0700

    Import sequelize from database file

--- models/cart.js
+++ models/cart.js
@@ -1,5 +1,8 @@
 const Seqeulize = require('sequelize');
 
+const sequelize = require('../util/database');
+
 
 // progress:
 // s11-166:Import Sequelize 
+// s11-166:Import sequelize from database file
