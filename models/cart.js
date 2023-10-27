84474c87308fb5a0f0a50251caa418b3167c29b6
isnendyankp <isnendyankp@gmail.com>
2023-10-27 08:17:17 +0700

    Export cart model

--- models/cart.js
+++ models/cart.js
@@ -11,9 +11,12 @@ const Cart = sequelize.define('cart', {
     }
 });
 
+module.exports = Cart;
+
 
 // progress:
 // s11-166:Import Sequelize 
 // s11-166:Import sequelize from database file
 // s11-166:Define Cart model with sequelize.define() & name it cart
 // s11-166:Add id property to cart model
+// s11-166:Export cart model
