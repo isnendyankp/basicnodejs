b5a691cf79002db3762f68d7abb7188216b20c6c
isnendyankp <isnendyankp@gmail.com>
2023-10-31 11:13:03 +0700

    Define CartItem model with sequelize.define()

--- models/cart-item.js
+++ models/cart-item.js
@@ -1,5 +1,12 @@
 const Seqeulize = require('sequelize');
 
+const sequelize = require('../util/database');
+
+const CartItem = sequelize.define('cartItem', {
+
+});
+
 
 // Progress:
 // s11-166:Import sequelize from database file
+// s11-166:Define CartItem model with sequelize.define() & name it cartItem
