3223c986d21194d52b7e68265b268089d6b270e4
isnendyankp <isnendyankp@gmail.com>
2023-11-07 12:43:21 +0700

    Fix code export .getCart

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,37 +54,20 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-      .getCart()
-      .then(cart => {
-        return cart.getProducts()
+    .getCart()
+    .then(cart => {
+      return cart
+        .getProducts()
         .then(products => {
           res.render('shop/cart', {
             path: '/cart',
-            pageTitle: 'Your Cart'
+            pageTitle: 'Your Cart',
             products: products
           });
         })
         .catch(err => console.log(err));
-      })
-      .catch(err => console.log(err));
-  // Cart.getCart(cart => {
-  //   Product.fetchAll(products => {
-  //     const cartProducts = [];
-  //     for (product of products) {
-  //       const cartProductData = cart.products.find(
-  //         prod => prod.id === product.id
-  //       );
-  //       if (cartProductData) {
-  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
-  //       }
-  //     }
-  //     res.render('shop/cart', {
-  //       path: '/cart',
-  //       pageTitle: 'Your Cart',
-  //       products: cartProducts
-  //     });
-  //   });
-  // });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
