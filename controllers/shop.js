f28ffff3551597334ae48e96ec90fb8caa6bc08a
isnendyankp <isnendyankp@gmail.com>
2023-11-20 13:33:57 +0700

    fix code export.postCart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
-const { get } = require('../routes/admin');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -73,10 +72,11 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then(cart => {
-      fetchedCart = cart; 
+      fetchedCart = cart;
       return cart.getProducts({ where: { id: prodId } });
     })
     .then(products => {
@@ -84,18 +84,18 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
+
       if (product) {
-        // ...
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findById(prodId)
-        .then(product => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity }
-          });
-          });
-        })
-        .catch(err => console.log(err));
+      return Product.findById(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
     })
     .then(() => {
       res.redirect('/cart');
