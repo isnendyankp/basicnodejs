18eba786d5bc7b85ef867ac80af8a9b3941d14d8
isnendyankp <isnendyankp@gmail.com>
2023-11-24 10:44:22 +0700

    create OrderItem model base

--- models/order-item.js
+++ models/order-item.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
+const OrderItem = sequelize.define('orderItem', { 
+   
+});
+
 
 // progress:
 // 11-171: import Sequelize and sequelize @models/order-item
+// 11-171: create OrderItem model base
