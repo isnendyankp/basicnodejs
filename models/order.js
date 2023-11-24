23d42c6a0302fea1ff0186c607c9f839ecdec296
isnendyankp <isnendyankp@gmail.com>
2023-11-24 10:42:06 +0700

    export Order model

--- models/order.js
+++ models/order.js
@@ -11,7 +11,10 @@ const Order = sequelize.define('order', {
     }
 });
 
+module.exports = Order;
+
 // progress:
 // 11-171: import Sequelize and sequelize
 // 11-171: create Order model base
 // 11-171: add id property to Order model
+// 11-171: export Order model
