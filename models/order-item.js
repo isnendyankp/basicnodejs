8c46cf00a8c6b8adc9ff335ad6524a6e903078e6
isnendyankp <isnendyankp@gmail.com>
2023-11-24 10:44:47 +0700

    add id property to OrderItem model

--- models/order-item.js
+++ models/order-item.js
@@ -3,10 +3,13 @@ const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const OrderItem = sequelize.define('orderItem', { 
-   
+   id: {
+
+   }
 });
 
 
 // progress:
 // 11-171: import Sequelize and sequelize @models/order-item
 // 11-171: create OrderItem model base
+// 11-171: add id property to OrderItem model
